3af24e6479993607963cf6441b5eb875aa7ca6da
Abhinav Prasai <abhinav.prsai@gmail.com>
2024-09-23 17:51:00 +0100

    fixed wm crashing when i close terminal: Just needed to remove destroywindow mask case

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <stdio.h>
@@ -6,109 +7,107 @@
 #include <unistd.h>
 
 #include "structs.h"
-#include "config.h"
 
 #define SCREEN_WIDTH    XDisplayWidth(dpy, DefaultScreen(dpy))
 #define SCREEN_HEIGHT   XDisplayHeight(dpy, DefaultScreen(dpy))
-#define TITLEBAR_HEIGHT	5
-
 
 /* Variables */
 Display *dpy;
 
 int GAPS_SIZE = 15;
+XColor BORDER_COLOUR;
 int BORDER_THICKNESS = 3;
+int TITLEBAR_HEIGHT = 5;
 int openWindows = 0;
 
+unsigned int NumLockMask = 0;
 
-/* Function Declarations*/
 
+/* Function Declarations */
 // Main Loop
-void Cases(XEvent *, KeyCode *);
+void Cases(XEvent *);
 void Configure(void);
 void Run(void);
 void InitialChecks(int, char *);
+void GrabKeys(void);
 
 // Requests
 void ConfigureWindowRequest(XEvent *);
 void HandleConfigureRequest(XWindowChanges *, XConfigureRequestEvent *);
 
 // Misc
-void Error(char *, int, int);
-
+void Error(const char *, int, int);
 
 /* Functions */
+
 void
 InitialChecks(int argc, char *argv)
 {
-	/* Initial checks to make sure the program can run */
-	if (!(dpy = XOpenDisplay(NULL)))
-		Error("Can't open the display\n", EXIT_FAILURE, True);
+    /* Initial checks to make sure the program can run */
+    if (!(dpy = XOpenDisplay(NULL)))
+        Error("Can't open the display\n", EXIT_FAILURE, true);
 }
 
 void
-Error(char *ErrorMessage, int ERROR_CODE, int EXIT)
+Error(const char *ErrorMessage, int ERROR_CODE, int EXIT)
 {
-	/* Outputs error message and dies 💀 */
-	fprintf(stderr, "%s", ErrorMessage);
-		exit(ERROR_CODE);
+    fprintf(stderr, "%s", ErrorMessage);
+    if (EXIT)
+        exit(ERROR_CODE);
 }
 
 void
-Configure()
+Configure(void)
 {
-	/* Configures the custom settings set by the user */
+    /* Configures the custom settings set by the user */
 }
 
 void
-Run()
+Run(void)
 {
-	/* Main loop of program */
-	Window root = DefaultRootWindow(dpy);
+    /* Main loop of the program */
+    Window root = DefaultRootWindow(dpy);
 
-	KeyCode enter = XKeysymToKeycode(dpy, XStringToKeysym("Return"));
-    XGrabKey(dpy, enter, Mod1Mask, root, True, GrabModeAsync, GrabModeAsync);
+	GrabKeys();
 
-	/* Configure();  TODO */
-	XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask | KeyPressMask);
+    XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask | KeyPressMask);
 
-	for(;;) {
-		XEvent e;
-		XNextEvent(dpy, &e);
-		Cases(&e, &enter);
-	}
+    for (;;) {
+        XEvent e;
+        XNextEvent(dpy, &e);
+        Cases(&e);
+    }
 }
 
 void
 ConfigureWindowRequest(XEvent *e)
 {
+	/* Configures the window before it is mapped */
     XWindowChanges changes;
     XConfigureRequestEvent *configRequest = &e->xconfigurerequest;
 
     configRequest->height = SCREEN_HEIGHT - ((GAPS_SIZE * 2) + (BORDER_THICKNESS * 2)) - TITLEBAR_HEIGHT;
     configRequest->width = SCREEN_WIDTH - ((GAPS_SIZE * 2) + (BORDER_THICKNESS * 2));
-
     configRequest->x = (SCREEN_WIDTH / 2) - (configRequest->width / 2);
     configRequest->y = (SCREEN_HEIGHT / 2) - (configRequest->height / 2) + TITLEBAR_HEIGHT;
-
     configRequest->border_width = BORDER_THICKNESS;
     configRequest->above = None;
     configRequest->detail = Above;
     configRequest->value_mask = CWX | CWY | CWWidth | CWHeight;
 
-	XSetWindowBorderWidth(dpy, e->xmaprequest.window, BORDER_THICKNESS);
+    XSetWindowBorderWidth(dpy, e->xmaprequest.window, BORDER_THICKNESS);
 
-	XColor color;
-	color.red = 255 * 256;
-	color.green = 0 * 256;
-	color.blue = 0 * 256;
-	color.flags = DoRed | DoGreen | DoBlue;
+    BORDER_COLOUR.red = 255 * 256;
+    BORDER_COLOUR.green = 0;
+    BORDER_COLOUR.blue = 0;
+    BORDER_COLOUR.flags = DoRed | DoGreen | DoBlue;
 
-	Colormap colormap = DefaultColormap(dpy, DefaultScreen(dpy));
-	if (XAllocColor(dpy, colormap, &color)) {
-		XSetWindowBorder(dpy, e->xmaprequest.window, color.pixel);
-	} else
-		fprintf(stderr, "Error: Failed to allocate color for window border\n");
+    Colormap colormap = DefaultColormap(dpy, DefaultScreen(dpy));
+    if (XAllocColor(dpy, colormap, &BORDER_COLOUR)) {
+        XSetWindowBorder(dpy, e->xmaprequest.window, BORDER_COLOUR.pixel);
+    } else {
+        fprintf(stderr, "Error: Failed to allocate color for window border\n");
+    }
 
     HandleConfigureRequest(&changes, configRequest);
 }
@@ -116,6 +115,7 @@ ConfigureWindowRequest(XEvent *e)
 void
 HandleConfigureRequest(XWindowChanges *changes, XConfigureRequestEvent *ev)
 {
+	/* Sets the changes */
     changes->x = ev->x;
     changes->y = ev->y;
     changes->width = ev->width;
@@ -124,46 +124,125 @@ HandleConfigureRequest(XWindowChanges *changes, XConfigureRequestEvent *ev)
     changes->sibling = ev->above;
     changes->stack_mode = ev->detail;
 
-            /* Set border width */
+    /* Set border width */
     XConfigureWindow(dpy, ev->window, ev->value_mask, changes);
 }
 
 void
-HandleKeyPress(XEvent *e, KeyCode key)
+SpawnWindow(const Arg *arg)
 {
-	if ((e->xkey.keycode == key && (e->xkey.state & Mod1Mask)) && fork() == 0) {
-		execlp("st", "", NULL);
-	//	exit(0);
+	if (!fork()) {
+		setsid();
+		execvp(((char **)arg->cmd)[0], (char **) arg->cmd);
+		fprintf(stderr, "execvp %s err", ((char **) arg->cmd)[0]);
+		exit(EXIT_SUCCESS);
 	}
 }
 
+/*
 void
-Cases(XEvent *e, KeyCode *enter)
+CloseWindow(const Arg *arg)
 {
+    Window focused;
+    int revert;
+
+    XGetInputFocus(dpy, &focused, &revert);
+    if (focused != None && focused != PointerRoot && focused != DefaultRootWindow(dpy)) {
+        XDestroyWindow(dpy, focused);
+    }
+}
+*/
+
+void
+GrabKeys(void)
+{
+    XUngrabKey(dpy, AnyKey, AnyModifier, DefaultRootWindow(dpy));
+
+    for (unsigned int i = 0; i < LENGTH(keys); ++i) {
+        KeyCode code = XKeysymToKeycode(dpy, keys[i].key);
+        XGrabKey(dpy, code, keys[i].mod, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+
+		// Grabbing numcock
+        if (NumLockMask) {
+            XGrabKey(dpy, code, keys[i].mod | NumLockMask, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+        }
+    }
+}
+
+unsigned int
+CleanMask(unsigned int mask)
+{
+    return mask & ~(NumLockMask | LockMask);
+}
+
+
+void
+HandleKeyPress(XEvent *e)
+{
+	/*  Handles the key presses */
+    KeySym keysym = XLookupKeysym(&e->xkey, 0);
+    unsigned int mod = CleanMask(e->xkey.state);
+
+    for (int i = 0; i < LENGTH(keys); ++i) {
+        if (keysym == keys[i].key && mod == CleanMask(keys[i].mod)) {
+            if (keys[i].fn) {
+                keys[i].fn(&(keys[i].arg));
+            }
+            break;
+        }
+    }
+}
+
+void
+Cases(XEvent *e)
+{
+	/* Switch case for the events, notifications ect */
     switch (e->type) {
 
-		default: break;
-        case DestroyNotify: { XDestroyWindow(dpy, e->xdestroywindow.window); break; }
+        case KeyPress:
+            HandleKeyPress(e);
+            break;
 
-        case KeyPress: {
-			HandleKeyPress(e, *enter);
-	  	}
-
-        case MapRequest: {
+        case MapRequest:
             ConfigureWindowRequest(e);
             XMapWindow(dpy, e->xmaprequest.window);
             break;
-		}
+
+        default:
+            break;
     }
+}
+
+void
+Setup(void)
+{
+	/* Main setup which gets the numlock keycode through bitwise operations */
+
+    XModifierKeymap *modmap = XGetModifierMapping(dpy);
+    KeyCode numlock_keycode = XKeysymToKeycode(dpy, XK_Num_Lock);
+
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < modmap->max_keypermod; ++j) {
+            if (modmap->modifiermap[i * modmap->max_keypermod + j] == numlock_keycode) {
+                NumLockMask = (1 << i);
+                break;
+            }
+        }
+    }
+
+    XFreeModifiermap(modmap);
 }
 
 int
 main(int argc, char *argv[])
 {
-	InitialChecks(argc, *argv);
-	Run();
+    InitialChecks(argc, *argv);
+	Setup();
 
+    Run();
 
-	XCloseDisplay(dpy);
-	return EXIT_SUCCESS;
+    XCloseDisplay(dpy);
+    return EXIT_SUCCESS;
 }
+
+
--- structs.h
+++ structs.h
@@ -1,11 +1,31 @@
-typedef struct _CustomCommand {
-	char *name;
-	char *args;
-} CustomCommand;
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#define MOD Mod4Mask
+#define LENGTH(X) (sizeof X / sizeof X[0])
 
-typedef struct _CustomBind {
-	bool mod;
-	bool shiftmask;
-	int	 key;
-	CustomCommand spawn;
-} CustomBind;
+
+typedef struct {
+    const char **cmd;
+	int ops;
+} Arg;
+
+typedef struct {
+	int mod;
+	KeySym key;
+    void (*fn) (const Arg *);
+    Arg arg;
+} Key;
+
+extern void SpawnWindow(const Arg *arg);
+// extern void CloseWindow(const Arg *arg);
+
+static const char *terminal[] = {"st", NULL};
+static const char *discord[] = {"discord", NULL};
+
+static const Key keys[] = {
+	{ MOD, XK_Return, SpawnWindow, {.cmd = terminal}},
+	{ MOD, XK_e, SpawnWindow, {.cmd = discord}},
+//	{ MOD | ShiftMask, XK_Q, CloseWindow, {NULL}},
+
+};
